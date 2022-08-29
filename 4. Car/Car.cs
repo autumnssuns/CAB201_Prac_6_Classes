56929eef75df53be4ab90d6d5a8e365b66d5572b
trand8 <trand8@MELT-GP-S518N>
2022-08-29 15:55:42 +1000

    Question 4

--- 4. Car/Car.cs
+++ 4. Car/Car.cs
@@ -8,10 +8,47 @@ namespace Car
     class Car
     {
         /// <summary>
+        /// The model of the car.
+        /// </summary>
+        string model;
+
+        /// <summary>
+        /// The fuel consumption in litres per km.
+        /// </summary>
+        double litresPerKm;
+
+        Car(string newModel, double newLitresPerKm)
+        {
+            model = newModel;
+            litresPerKm = newLitresPerKm;
+        }
+
+        Car(string newModel)
+        {
+            model = newModel;
+            litresPerKm = 50;
+        }
+
+        Car()
+        {
+            Console.WriteLine("No model was found, please enter one:");
+            model = Console.ReadLine();
+            litresPerKm = 50;
+        }
+
+        /// <summary>
         /// The main program.
         /// </summary>
         static void Main(string[] args)
         {
+            Car mazda = new Car("Mazda 3", 7);
+            Console.WriteLine($"The model {mazda.model} consumes {mazda.litresPerKm} litres of fuel per km.");
+
+            Car honda = new Car("Honda");
+            Console.WriteLine($"The model {honda.model} consumes {honda.litresPerKm} litres of fuel per km.");
+
+            Car unknownCar = new Car();
+            Console.WriteLine($"The model {unknownCar.model} consumes {unknownCar.litresPerKm} litres of fuel per km.");
         }
     }
 }
