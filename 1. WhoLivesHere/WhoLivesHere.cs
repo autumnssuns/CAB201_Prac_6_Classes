96612579582dcb25b19033031bc40253f784731f
trand8 <trand8@MELT-GP-S518N>
2022-08-29 15:28:47 +1000

    Question 1

--- 1. WhoLivesHere/WhoLivesHere.cs
+++ 1. WhoLivesHere/WhoLivesHere.cs
@@ -8,10 +8,30 @@ namespace WhoLivesHere
     class WhoLivesHere
     {
         /// <summary>
+        /// The name of the person.
+        /// </summary>
+        string name;
+
+        /// <summary>
+        /// The city the person lives in.
+        /// </summary>
+        string city;
+
+        /// <summary>
         /// The main program.
         /// </summary>
         static void Main(string[] args)
         {
+            WhoLivesHere andy = new WhoLivesHere();
+            andy.name = "Andy";
+            andy.city = "Brisbane";
+
+            WhoLivesHere kevin = new WhoLivesHere();
+            kevin.name = "Kevin";
+            kevin.city = "Melbourne";
+
+            Console.WriteLine($"{andy.name} lives in {andy.city}");
+            Console.WriteLine($"{kevin.name} lives in {kevin.city}");
         }
     }
 }
