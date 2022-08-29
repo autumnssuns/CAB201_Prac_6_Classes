7e2f713ccd1473d2aba9182ef242333dc597f9fa
MELTUser <MELTUser@MELT-GP-S503N>
2022-08-29 17:30:31 +1000

    Question 1

--- 1. WhoLivesHere/WhoLivesHere.cs
+++ 1. WhoLivesHere/WhoLivesHere.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WhoLivesHere
 {
@@ -7,11 +8,39 @@ namespace WhoLivesHere
     /// </summary>
     class WhoLivesHere
     {
+        string name;
+        string city;
+        int age;
+
         /// <summary>
         /// The main program.
         /// </summary>
         static void Main(string[] args)
         {
+            List<WhoLivesHere> list = new List<WhoLivesHere>();
+
+            WhoLivesHere superTutor = new WhoLivesHere();
+            superTutor.name = "Lawrence";
+            superTutor.city = "Brisbane";
+            superTutor.age = 50;
+            list.Add(superTutor);
+
+            WhoLivesHere visitor = new WhoLivesHere();
+            visitor.name = "Timothy";
+            visitor.city = "Adelaide";
+            visitor.age = 42;
+            list.Add(visitor);
+
+            WhoLivesHere tutor = new WhoLivesHere();
+            tutor.name = "Dan";
+            tutor.city = "Sydney";
+            tutor.age = 21;
+            list.Add(tutor);
+
+            foreach (WhoLivesHere person in list)
+            {
+                Console.WriteLine($"{person.name} lives in {person.city}");
+            }
         }
     }
 }
