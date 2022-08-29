d8abf8280e3a1060afccf1006e39c341185c1b61
MELTUser <MELTUser@MELT-GP-S503N>
2022-08-29 17:48:59 +1000

    Question 4

--- 4. Car/Car.cs
+++ 4. Car/Car.cs
@@ -4,14 +4,35 @@ namespace Car
 {
     /// <summary>
     /// A class to hold details of a car
+    /// The Car should contain two fields, a model and a litres per kilometre.
     /// </summary>
     class Car
     {
+        string model;
+        int litresPerKm;
+
+        Car(string model, int litresPerKm)
+        {
+            this.model = model;
+            this.litresPerKm = litresPerKm;
+        }
+
+        Car(string model)
+        {
+            this.model = model;
+            litresPerKm = 50;
+        }
+
         /// <summary>
         /// The main program.
         /// </summary>
         static void Main(string[] args)
         {
+            Car mazda = new Car("Mazda 3", 7);
+            Console.WriteLine($"{mazda.model} consumes {mazda.litresPerKm} litres / km");
+
+            Car toyota = new Car("Toyota");
+            Console.WriteLine($"{toyota.model} consumes {toyota.litresPerKm} litres / km");
         }
     }
 }
