09ea8e0c441f86414ace06c702312166f6837e24
MELTUser <MELTUser@MELT-GP-S503N>
2022-08-29 18:22:09 +1000

    Question 6

--- 6. HousePlant/HousePlant.cs
+++ 6. HousePlant/HousePlant.cs
@@ -7,13 +7,33 @@ namespace HousePlant
     /// </summary>
     class HousePlant
     {
-        /// <summary>
-        /// The main program.
-        /// </summary>
-        static void Main(string[] args)
+        public string name { get; }
+
+        double price;
+        public double Price { 
+            get
+            {
+                price++;
+                return price;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid price");
+                } else
+                {
+                    price = value;
+                }
+            }
+        }
+        public bool isFed;
+
+        public HousePlant(string name, double price, bool isFed)
         {
-
-
+            this.name = name;
+            this.price = price;
+            this.isFed = isFed;
         }
     }
 }
---
+++ 6. HousePlant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousePlant
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The main program.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            HousePlant plant = new HousePlant("Philodendron", 29.99, true);
+            plant.isFed = false;
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+            Console.WriteLine($"{plant.name}, {plant.Price}, fed: {plant.isFed}");
+        }
+    }
+}
