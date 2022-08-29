f391d93a7b30ea1afa5b3b3ed185fda9c4ffdfef
trand8 <trand8@MELT-GP-S518N>
2022-08-29 16:50:42 +1000

    Question 8

--- 8. Number/Number.cs
+++ 8. Number/Number.cs
@@ -7,6 +7,21 @@ namespace Number
     /// </summary>
     class MyNumber
     {
+        public int myNumber { get; set; }
 
+        public int Square
+        {
+            get { return myNumber * myNumber; }
+        }
+
+        public int Cube
+        {
+            get { return Square * myNumber; }
+        }
+
+        public MyNumber(int myNumber)
+        {
+            this.myNumber = myNumber;
+        }
     }
 }
---
+++ 8. Number/TestNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    internal class TestNumber
+    {
+        MyNumber myNumber { get; set; }
+
+        TestNumber(int number)
+        {
+            myNumber = new MyNumber(number);
+        }
+
+        static void Main(string[] args)
+        {
+            TestNumber tester = new TestNumber(48);
+            Console.WriteLine(tester.myNumber.Square);
+            tester.myNumber = new MyNumber(36);
+            Console.WriteLine(tester.myNumber.Square);
+        }
+    }
+}
